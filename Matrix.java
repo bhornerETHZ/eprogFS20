abce7fbc88a99123a6729623441194afd090276f
bhornerETHZ <76952487+bhornerETHZ@users.noreply.github.com>
2021-01-06 14:06:10 +0100

    Create Matrix.java

---
+++ Matrix.java
@@ -0,0 +1,15 @@
+//Exercise 2
+package eprogFS20;
+
+public class Matrix {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static boolean checkMatrix(int[][] m) {
+		//TODO
+		return false;
+	}
+}
