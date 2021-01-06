b6ac00221e6085c7ca564478f77a3b830d814e19
bhornerETHZ <76952487+bhornerETHZ@users.noreply.github.com>
2021-01-06 13:40:44 +0100

    Create Scrabble.java

---
+++ Scrabble.java
@@ -0,0 +1,16 @@
+// Exercise 1
+package eprogFS20;
+
+public class Scrabble {
+
+	public static void main(String[] args) {
+		drawNameSquare("alfred");
+
+	}
+
+	public static void drawNameSquare(String name) {
+		// TODO
+
+	}
+
+}
